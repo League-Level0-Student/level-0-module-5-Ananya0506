ae441210a84f6ad95a962715ea997f42c856f2b3
league <league@imac-14.attlocal.net>
2023-03-20 16:56:25 -0700

    starting amazing rings

--- src/_01_algorithms/_2_fibonacci/Fibonacci.java
+++ src/_01_algorithms/_2_fibonacci/Fibonacci.java
@@ -1,5 +1,21 @@
 package _01_algorithms._2_fibonacci;
 
 public class Fibonacci {
+public static void main(String[] args) {
+	
+	int num1 = 0;
+	int num2 = 1;
+	int sum = 0;
+	
+	System.out.print(num1+ " ");
+	System.out.print(num2+ " ");
+	for (int i=0; i<10; i++) {
+	sum= num1+num2;
+	
+System.out.print((num1 + num2)+ " " );	
 
+num1 = num2;
+num2 = sum; 
+	}
+}
 }
