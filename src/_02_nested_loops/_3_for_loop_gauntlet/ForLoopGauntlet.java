abe9dc9d0d1825221c18f3f84dbea91ffffcd125
league <league@192.168.1.86>
2023-04-10 16:58:01 -0700

    am on #3 nested for loops For Loop Gauntlet

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/ForLoopGauntlet.java
@@ -0,0 +1,42 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class ForLoopGauntlet {
+
+	public static void main(String[] args) {
+	// NESTED FOR LOOPS
+		
+	// 1.	
+	//for(int i=0; i<3; i++) {
+	//	for(int a = 0; a<3; a++) {
+	//		System.out.println(i+" "+ a);
+			
+			
+	//	}
+	//}
+		
+	// 2.
+//for(int a = 0; a<3; a++) {	
+//for(int i=1; i<4; i++) {
+//	System.out.print(i+(a*3));
+//}
+//System.out.println();
+//}
+	
+	//3.
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	}
