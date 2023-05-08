5fe6fc3282df9c4191cc9a22ee4f4593a4e45188
league <league@imac-14.attlocal.net>
2023-05-08 16:57:29 -0700

    am on skill practice

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -8,19 +8,36 @@ public class HappyPet {
 	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-    String pet = JOptionPane.showInputDialog("what pet do you want to buy? type the number corresponding to the animal. 1. dog; 2. cat; 3. horse; 4. parrot");
+    String pet = JOptionPane.showInputDialog("what pet do you want to buy? dog; cat; hamster; horse; parrot");
     		
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
 		
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "What task do you want to do for your pet?", "Task", 0, JOptionPane.INFORMATION_MESSAGE, null,
+for(int i = 0; i<10; i++) {
+    
+    int task = JOptionPane.showOptionDialog(null, "What task do you want to do for your " + pet, "Task for" + pet, 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "food", "water", "groom" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
+if (pet.equalsIgnoreCase("dog")) {
+	dog();
+} else if (pet.equalsIgnoreCase("cat")) {
+	cat();
+} else if (pet.equalsIgnoreCase("horse")) {
+	horse();
+} else if (pet.equalsIgnoreCase("hamster")) {
+	hamster();
+} else if (pet.equalsIgnoreCase("parrot")) {
+	parrot();
+}
 
-			// 7. If you determine the happiness level is large enough, tell the
+}
+			JOptionPane.showMessageDialog(null,"great job! you have finished the game! your pet's happiness level is " +  happinessLevel + "! have a nice day!" );
+System.exit(0);
+
+// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
 	}
@@ -28,4 +45,46 @@ public class HappyPet {
 	// 5. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+
+ public static void dog() {
+	 JOptionPane.showMessageDialog(null,"your dog is happy. bark!");
+	 happinessLevel++;
+	}
+ public static void cat() {
+	 JOptionPane.showMessageDialog(null,"your cat is happy. purr!");
+	 happinessLevel++;
+	}
+ public static void horse() {
+	 JOptionPane.showMessageDialog(null,"your horse is happy. neigh!");
+	 happinessLevel++;
+	}
+ public static void hamster() {
+	 JOptionPane.showMessageDialog(null,"your hamster is happy. squeak!");
+	 happinessLevel++;
+	}
+ public static void parrot() {
+	 JOptionPane.showMessageDialog(null,"your parrot is happy. squawk!");
+	 happinessLevel++;
+	}
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- src/_03_method_writing/_4_calculator/Calculator.java
+++ src/_03_method_writing/_4_calculator/Calculator.java
@@ -14,18 +14,52 @@ public class Calculator {
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "divide", "multiply", "subtract", "add" }, null);
 
 		// 3) Call the correct method depending on what option the user chooses
+double num = 0;
 		
+		if (task==3) {
+			num = add(number1, number2);
+result(num);		
+		} else if (task==2) {
+			num = subtract(number1, number2);
+		result(num);
+		} else if (task==1) {
+			num = multiply(number1, number2);
+		result(num);
+		} else if (task==0) {
+			num = divide(number1, number2);
+	result(num);
+		}
+		
+
 		// 4) Call the result() method and put the answer in a pop-up
+
+		
+		
 	}
-	
 	// 1) Make 4 static methods (add, subtract, multiply, divide)
 	//		They should each take 2 numbers as parameters
 	//		and return the answer.
-	 
-	
+	 public static double add( int number1 , int  number2) {
+		 double nummy = number1 + number2;
+return nummy;	
+	 }
+	 public static double multiply( int number1 , int  number2) {
+		 double nummy = number1 * number2;
+	 return nummy;
+	 }
+	 public static double subtract( int number1 , int  number2) {
+		  double nummy = number1 - number2;
+	 return nummy;
+	 }
+	 public static double divide( int number1 , int  number2) {
+		   double nummy = number1 / number2;
+	 return nummy;
+	 }
 	// 2) Make a static method called " result" that takes a number
 	//		as a parameter and returns the number in a string
 	//		EX: "Your answer is " + number;
 
-	
+	 public static void result(double nummy) {
+	 JOptionPane.showMessageDialog(null, "your answer is: " + nummy);
+	 }
 }
